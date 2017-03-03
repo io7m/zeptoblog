39bd9eb2471282aaf8332d3c66ebd8a0d22e708f
Mark Raynsford <code@io7m.com>
2017-03-03 15:36:21 +0000

    Implement post serialization

---
+++ com.io7m.zeptoblog.core/src/main/java/com/io7m/zeptoblog/core/ZBlogPostSerializerType.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2017 <code@io7m.com> http://io7m.com
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package com.io7m.zeptoblog.core;
+
+import javaslang.collection.Seq;
+import javaslang.control.Validation;
+
+/**
+ * The type of blog post serializers.
+ */
+
+public interface ZBlogPostSerializerType
+{
+  /**
+   * Serialize the blog post to a string.
+   *
+   * @param post The blog post
+   *
+   * @return A string, or a list of serialization errors on failure
+   */
+
+  Validation<Seq<ZError>, String> serialize(
+    ZBlogPost post);
+}
